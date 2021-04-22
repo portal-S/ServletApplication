ea1b91da4a7445b6683a1882905984cac5ca2d1e
portal-S <aligus321@mail.ru>
2021-04-22 19:46:44 +0300

    fileCommit2

--- src/main/java/com/example/WebApp/HelloServlet.java
+++ src/main/java/com/example/WebApp/HelloServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.annotation.*;
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
     private String message;
-    private File file = new File("src/main/java/com/portal/repository");
+    private File file = new File("src/main/resources/files");
 
 
     public void init() {
