a3333b89b1e06a57d7f7a058cee6783d344cb36c
portal-S <aligus321@mail.ru>
2021-04-23 16:05:12 +0300

    2

--- src/main/java/com/example/WebApp/Servlet.java
+++ src/main/java/com/example/WebApp/Servlet.java
@@ -1,10 +1,14 @@
 package com.example.WebApp;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.portal.model.User;
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
+import javax.json.Json;
+import javax.json.JsonBuilderFactory;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -24,9 +28,11 @@ public class Servlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
         String id = request.getParameter("id");
         String name = request.getParameter("name");
         User user = new User(Integer.parseInt(id), name);
-        response.getWriter().write(user.toString());
+        response.getWriter().write(gson.toJson(user));
     }
 }
